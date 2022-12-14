470810186e2052961a6aa1601b8a6bcef7ce1093
tomasjur <tomasjkaunas@gmail.com>
2022-12-14 14:06:51 +0200

    Navbar layout and Home page

---
+++ reservations-system/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<p>Hi, this is the Tables Reservation system.</p>
+<ul>
+    <li>In the <b>Restaurants</b> page you can add a new restaurant.</li>
+    <li>In the <b>Reservations list</b> page you can see your already made reservations list.</li>
+    <li>In the <b>Make a Reservation</b> page you can book a table in any of the restaurants.</li>
+</ul>
+@stop
---
+++ reservations-system/resources/views/layout.blade.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <style>
+        .content {
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link" href="#">Restaurants</a>
+                    <a class="nav-link" href="#">Reservations list</a>
+                    <a class="nav-link" href="#">Make a Reservation</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+
+</html>
