411327142024edfcb83aa0daf2413e8cecae0d19
tomasjur <tomasjkaunas@gmail.com>
2022-12-19 14:22:18 +0200

    Home edit

--- reservations-system/resources/views/home.blade.php
+++ reservations-system/resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <p>Hi, this is the Tables Reservation system.</p>
 <ul>
-    <li>In the <b>Restaurants</b> page you can add a new restaurant.</li>
-    <li>In the <b>Reservations list</b> page you can see your already made reservations list.</li>
-    <li>In the <b>Make a Reservation</b> page you can book a table in any of the restaurants.</li>
+    <li>In the <b>Restaurants</b> page you can add a new restaurant and see all restaurants list with tables.</li>
+    <li>In the <b>Reservations</b> page you can make a new reservation and see reservations list.</li>
 </ul>
+<p>Made by Tomas Jurevicius.</p>
 @stop
